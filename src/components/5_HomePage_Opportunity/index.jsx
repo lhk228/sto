7e38efeec2494697ad0061c96092c74ab72d203d
ShahzodTuraev <110082755+ShahzodTuraev@users.noreply.github.com>
2023-04-15 12:38:13 +0900

    Update index.jsx

--- src/components/5_HomePage_Opportunity/index.jsx
+++ src/components/5_HomePage_Opportunity/index.jsx
@@ -19,45 +19,45 @@ const OpportunityPage = () => {
         <Introbox>
           <IntroImg src={introPic}></IntroImg>
           <IntroTextBox>
-            <IntroTitle>ClickFunnels는 "퍼널(Funnels)"을 통해 고객들이 여러분을 찾아오도록 도와줍니다!</IntroTitle>
+            <IntroTitle>메버는 "3D홈페이지 • AI마케팅"을 통해 고객들이 여러분을 찾아오도록 도와줍니다!</IntroTitle>
             <ResponsiveImg src={introPic}/>
             <TabletImg src={introPic}/>
-            <IntroText>우리는 최초의 퍼널 빌더이자, 페이스북, 구글, 유튜브, 인스타그램 또는 틱톡과 같은 사이트에서 사용자를 모아서 당신의 상점으로 이끌어주는 첫 번째 플랫폼입니다!
+            <IntroText>우리는 최초의 웹3D 빌더이자 페이스북, 구글, 유튜브, 인스타그램 또는 틱톡과 같은 사이트에서 사용자를 모아서 당신의 상점으로 이끌어주는 AI마케팅 플랫폼이기도 합니다!
             </IntroText>
-            <IntroText>만약 당신이 페이스북, 구글, 유튜브, 인스타그램, 혹은 틱톡에서 광고를 클릭해본 적이 있다면, 그 광고를 만든 사람이 제대로 했다면 아마도 당신이 바로 이동한 페이지가 ClickFunnels의 한 퍼널 중 하나였을 가능성이 높습니다!</IntroText>
-            <IntroText>그리고 이제야 알아채셨군요! 당신은 지금 ClickFunnels의 펀넬 속에 있습니다! (네, 이들은 진짜로 동작합니다!)</IntroText>
+            <IntroText>만약 당신이 페이스북, 구글, 유튜브, 인스타그램, 혹은 틱톡에서 광고를 클릭해본 적이 있다면, 그 광고를 만든 사람 중 한팀이 우리 일 수 있습니다!</IntroText>
+            <IntroText>그리고 지금까지 경험하지 못한 3D홈페이지에서, 높은 매출 전환률과 체류시간을 경험할 수 있습니다! 그리고 이는 다양한 특허망을 갖고 있는 메버만이 할 수 있습니다. </IntroText>
           </IntroTextBox>
         </Introbox>
-        <QuesText>그러나 ClickFunnels가 하는 일은 그것이 전부는 아닙니다 ...</QuesText>
-        <Title>ClickFunnels은 온라인 제품을 마케팅하고 판매하며 배송하는 데 필요한 모든 것을 제공합니다 </Title>
-        <AdHead>하나로 모아진 모든 것!</AdHead>
+        <QuesText>메버가 하는 일은 3D홈페이지 • AI마케팅이 전부는 아닙니다.</QuesText>
+        <Title>메버는 대한민국 CEO들이 글로벌 온라인 경쟁력을 갖추고 전세계적인 영향력을 행사할 수 있게 돕습니다. </Title>
+        <AdHead>CEO를 위한 모든 것!</AdHead>
         <Icon.Down/>
 
         <FunctionCard>
           <IntroTextBox>
-            <Suptitle>ClickFunnels는...</Suptitle>
-            <IntroTitle>당신의 웹사이트</IntroTitle>
-            <IntroText>웹사이트나 블로그를 만드는 것은 예전에는 지치는 일이었습니다. 보통 디자인하고 개발하는 데 걸리는 시간이 오래 걸렸는데 이제는 하루만에 해결할 수 있습니다!</IntroText>
-            <BoxUnit>FunnelHubs를 사용하면...</BoxUnit>
+            <Suptitle>메버는...</Suptitle>
+            <IntroTitle>당신의 3D웹사이트•AI콘텐츠</IntroTitle>
+            <IntroText>웹사이트나 콘텐츠를 만드는 것은 오랜 시간과 비용이 들어갑니다. 하지만, 이제는 단 몇시간, 몇분만에 해결할 수 있습니다!</IntroText>
+            <BoxUnit>메버을 사용하면...</BoxUnit>
             <CanWrapper>
               <Icon.Right/>
-              <CanText>매우 빠른 페이지 로딩 속도를 경험하여, 더 많은 전환과 판매를 이룰 수 있습니다</CanText>
+              <CanText>3D웹을 고객에게 제공하여 매우 색다른 경험을 선사하여, 더 많은 전환과 판매를 이룰 수 있습니다</CanText>
             </CanWrapper>
             <CanWrapper>
               <Icon.Right/>
-              <CanText>전적으로 맞춤화 가능하며, 컴퓨터 기술에 관계없이 누구나 사용할 수 있는 드래그 앤 드롭 페이지 빌더</CanText>
+              <CanText>CEO업종별 맞춤화가 가능하며, AI 3D기술에 관계없이 누구나 사용할 수 있는 자동 솔루션을 추구합니다</CanText>
             </CanWrapper>
             <CanWrapper>
               <Icon.Right/>
-              <CanText>내용을 입력하고 스위치를 누르면 즉시 사용 가능한 수십 개의 아름다운 페이지 템플릿이 준비되어 있습니다</CanText>
+              <CanText>내용을 입력하고 버튼을 누르면 즉시 사용 가능한 3D홈페이지와 AI컨텐츠를 경험하실 수 있습니다</CanText>
             </CanWrapper>
-            <CanText>ClickFunnels는 다음을 대체합니다:</CanText>
+            <CanText>메버는 이 회사들과 함께합니다:</CanText>
             <Icon.Brand/>
           </IntroTextBox>
           <CanImgBox>
             <CanImg src={canImg_1}/> 
-            <Button onClick={onClick} >지금 무료 평가판을 시작하세요</Button>
-            <Index>계약 없음 • 언제든지 해지 가능</Index>
+            <Button onClick={onClick} >처음 오신 CEO를 위한, 서비스 견적</Button>
+            <Index>약정 계약 없음 • 언제든지 해지 가능</Index>
           </CanImgBox>
         </FunctionCard>
 
