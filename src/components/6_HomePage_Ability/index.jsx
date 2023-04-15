fb02d37f135e50bd3cfecde96a97a2ccdb825102
ShahzodTuraev <110082755+ShahzodTuraev@users.noreply.github.com>
2023-04-15 12:41:57 +0900

    Update index.jsx

--- src/components/6_HomePage_Ability/index.jsx
+++ src/components/6_HomePage_Ability/index.jsx
@@ -17,27 +17,27 @@ const AbilityPage = () => {
   return (
     <Container>
       <Main>
-        <Title>ClickFunnels는 온라인 성공에 필요한 모든 것을 제공합니다.</Title>
-        <QuesText>고객이 귀사를 찾도록 유도하는 플러스 퍼널!</QuesText>
+        <Title>메버는 CEO들이 온라인 성공에 필요한 모든 것을 제공합니다.</Title>
+        <QuesText>고객이 귀사 웹을 찾도록 유도하는 메버!</QuesText>
         <Wrapper>
             <Box onClick={onBox}>
               <Icon.Sales/>
-              <Item>판매 퍼널</Item>
+              <Item>3D 마케팅 설계</Item>
               <Text>온라인 방문자를 실제 구매 고객으로 전환합니다.</Text>
             </Box>
             <Box onClick={onBox}>
               <Icon.Website/>
-              <Item>웹사이트</Item>
+              <Item>3D 웹사이트</Item>
               <Text>당신이 원하는 방식으로 완벽한 웹 프리즌스를 만듭니다.</Text>
             </Box>
             <Box onClick={onBox}>
               <Icon.Online/>
-              <Item>온라인 코스</Item>
+              <Item>CEO 교육과정</Item>
               <Text>당신의 지식, 열정 또는 경험을 수익으로 전환합니다.</Text>
             </Box>
             <Box onClick={onBox}>
               <Icon.Store/>
-              <Item>전자상거래 스토어</Item>
+              <Item>전자상거래 3D스토어</Item>
               <Text>온라인 제품 판매를 위한 온라인 스토어를 만듭니다. </Text>
             </Box>
             <Box onClick={onBox}>
@@ -47,12 +47,12 @@ const AbilityPage = () => {
             </Box>
             <Box onClick={onBox}>
               <Icon.Landing/>
-              <Item>방문 페이지</Item>
+              <Item>랜딩 페이지</Item>
               <Text>당신의 웹사이트나 퍼널을 위한 랜딩 페이지를 만듭니다.</Text>
             </Box>
             <Box onClick={onBox}>
               <Icon.Membership/>
-              <Item>회원 사이트</Item>
+              <Item>1.10.100억 매출챌린지</Item>
               <Text>반복적인 수익을 창출하기 위해 멤버십 사이트를 구축합니다.</Text>
             </Box>
             <Box onClick={onBox}>
@@ -67,7 +67,7 @@ const AbilityPage = () => {
             </Box>
             <Box onClick={onBox}>
               <Icon.Blog/>
-              <Item>블로그</Item>
+              <Item>AI콘텐츠</Item>
               <Text>당신의 산업에서 전문가로 인식되도록 블로그를 작성하세요.</Text>
             </Box>
             <Box onClick={onBox}>
@@ -82,7 +82,7 @@ const AbilityPage = () => {
             </Box>
             <Box onClick={onBox}>
               <Icon.Click/>
-              <Item>Funnels 편집기</Item>
+              <Item>메버 편집기</Item>
               <Text>쉽고 강력한 드래그 앤 드롭 페이지 편집 경험을 제공합니다.</Text>
             </Box>
             <Box onClick={onBox}>
@@ -97,22 +97,19 @@ const AbilityPage = () => {
             </Box>
             <Box onClick={onBox}>
               <Icon.Shopping/>
-              <Item>쇼핑 카트</Item>
+              <Item>꿈의고객 모임</Item>
               <Text>출시 예정…</Text>
             </Box>
         </Wrapper>
         <AdBox>
           <AdImg src={Logo}/>
-          <AdTitle>ClickFunnels를 무료로 사용해 보세요!</AdTitle>
-          <AdText>ClickFunnels를 적극적으로 사용하여 제품과 메시지를 전 세계에 쉽게 알리고 있는 100,000명 이상의 크리에이터 및 기업가와 함께하세요!</AdText>
-          <Button onClick={onClick} >오늘 무료 평가판을 시작하세요</Button>
-          <Index color='#7690A9' >계약 없음 • 언제든지 해지 가능</Index>
+          <AdTitle>메버를 무료로 경험해 보세요!</AdTitle>
+          <AdText>메버를 적극적으로 사용하여 제품과 메시지를 전 세계에 쉽게 알리고 있는 CEO 및 유튜버들과 함께하세요!</AdText>
+          <Button onClick={onClick} >처음 오신 CEO를 위한, 서비스 견적</Button>
+          <Index color='#7690A9' >약적 계약 없음 • 언제든지 해지 가능</Index>
         </AdBox>
-
       </Main> 
     </Container>
-    
-
   )
 }
 
